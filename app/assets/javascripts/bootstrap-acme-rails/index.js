cd1c11cc22839e7e7feacad0444d83ef55a2cd74
Jeremy Maziarz <jmaziarz@mitre.org>
2013-06-28 16:12:16 -0400

    Moved all JavaScript, image, and font asset to bootstrap-acme-rails subdirectories; Updated javascripts manifest file to reference bootstrap-acme-rails subdirectory

---
+++ app/assets/javascripts/bootstrap-acme-rails/index.js
@@ -0,0 +1,45 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require bootstrap-acme-rails/jquery-migrate-1.0.0.min
+//= require bootstrap-acme-rails/jquery-ui-1.10.0.custom.min
+//= require bootstrap-acme-rails/jquery.ui.touch-punch
+//= require bootstrap-acme-rails/modernizr
+//= require bootstrap-acme-rails/bootstrap
+//= require bootstrap-acme-rails/jquery.cookie
+//= require bootstrap-acme-rails/fullcalendar.min
+//= require bootstrap-acme-rails/jquery.dataTables.min
+//= require bootstrap-acme-rails/excanvas
+//= require bootstrap-acme-rails/jquery.flot
+//= require bootstrap-acme-rails/jquery.flot.pie
+//= require bootstrap-acme-rails/jquery.flot.stack
+//= require bootstrap-acme-rails/jquery.flot.resize.min
+//= require bootstrap-acme-rails/gauge.min
+//= require bootstrap-acme-rails/jquery.chosen.min
+//= require bootstrap-acme-rails/jquery.uniform.min
+//= require bootstrap-acme-rails/jquery.cleditor.min
+//= require bootstrap-acme-rails/jquery.noty
+//= require bootstrap-acme-rails/jquery.elfinder.min
+//= require bootstrap-acme-rails/jquery.raty.min
+//= require bootstrap-acme-rails/jquery.iphone.toggle
+//= require bootstrap-acme-rails/jquery.uploadify-3.1.min
+//= require bootstrap-acme-rails/jquery.gritter.min
+//= require bootstrap-acme-rails/jquery.imagesloaded
+//= require bootstrap-acme-rails/jquery.masonry.min
+//= require bootstrap-acme-rails/jquery.knob.modified
+//= require bootstrap-acme-rails/jquery.sparkline.min
+//= require bootstrap-acme-rails/counter
+//= require bootstrap-acme-rails/justgage.1.0.1.min
+//= require bootstrap-acme-rails/retina
+//= require bootstrap-acme-rails/core
+//= require bootstrap-acme-rails/charts
+//= require bootstrap-acme-rails/custom
